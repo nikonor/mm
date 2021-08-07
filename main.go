324cf079e1bea646b61f637ec4ae809831c03904
Leonid Nikolaev <nikonor@nikonor.ru>
2021-08-07 18:02:55 +0300

    [v] убрано лишнее

--- main.go
+++ main.go
@@ -28,13 +28,13 @@ const (
 )
 
 var (
-	mock                   map[string]*Mock
-	dirFlag                *string
-	portFlag, nextFlag, nn *int
-	l                      sync.RWMutex
-	nl                     sync.Mutex
-	macros                 []Macro
-	partSplitRE            *regexp.Regexp
+	mock               map[string]*Mock
+	dirFlag            *string
+	portFlag, nextFlag *int
+	l                  sync.RWMutex
+	nl                 sync.Mutex
+	macros             []Macro
+	partSplitRE        *regexp.Regexp
 )
 
 type Macro struct {
@@ -43,7 +43,7 @@ type Macro struct {
 	F          func() []byte
 }
 
-func uuidv4() []byte     { return []byte(uuid.New().String()) }
+func uuidV4() []byte     { return []byte(uuid.New().String()) }
 func getRndInt() []byte  { return []byte(strconv.FormatInt(int64(rand.Int31()), 10)) }
 func getMongoId() []byte { return []byte(newMongoID()) }
 func getNextInt() []byte {
@@ -64,12 +64,12 @@ func init() {
 	macros = append(macros, Macro{
 		Macro:      []byte("%v_uuid4%"),
 		IsVariadic: true,
-		F:          uuidv4,
+		F:          uuidV4,
 	})
 	macros = append(macros, Macro{
 		Macro:      []byte("%uuid4%"),
 		IsVariadic: false,
-		F:          uuidv4,
+		F:          uuidV4,
 	})
 	macros = append(macros, Macro{
 		Macro:      []byte("%increment%"),
@@ -122,8 +122,10 @@ func main() {
 	portFlag = flg.Int("p", 8888, "порт, на котором запустится мок")
 	nextFlag = flg.Int("n", 1, "первое число для последовательности")
 
-	flg.Parse(os.Args[1:])
-	nn = nextFlag
+	if err := flg.Parse(os.Args[1:]); err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+		return
+	}
 
 	mock = make(map[string]*Mock)
 	mock[NotFound] = &Mock{
@@ -135,8 +137,9 @@ func main() {
 	println("dir=", *dirFlag, ", port=", *portFlag)
 
 	handler := &H{}
-	http.ListenAndServe(":"+strconv.Itoa(*portFlag), handler)
-
+	if err := http.ListenAndServe(":"+strconv.Itoa(*portFlag), handler); err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+	}
 }
 
 type H struct {
@@ -145,18 +148,24 @@ type H struct {
 func (h *H) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	var err error
 
+	lp := randString(8)
+
+	fmt.Println(lp + "::" + req.RequestURI + "::call")
+
 	l.RLock()
 	m, ok := mock[req.RequestURI]
 	l.RUnlock()
 
 	if !ok {
+		fmt.Println(lp + "::" + req.RequestURI + "::mock not found, try get file")
 		m, err = makeMock(req.RequestURI)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", err)
+			fmt.Println(lp + "::" + req.RequestURI + "::new mock was not create => 404")
 			l.RLock()
 			m = mock[NotFound]
 			l.RUnlock()
 		} else {
+			fmt.Println(lp + "::" + req.RequestURI + "::new mock was create")
 			l.Lock()
 			mock[req.RequestURI] = m
 			l.Unlock()
@@ -165,13 +174,18 @@ func (h *H) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 
 	if m.Headers != nil && len(m.Headers) > 0 {
 		for k, v := range m.Headers {
+			fmt.Println(lp + "::" + req.RequestURI + "::add header::" + k + "=>" + v)
 			resp.Header().Add(k, v)
 		}
 	}
 	resp.WriteHeader(m.Code)
 	if m.Body != nil {
 		b := fillVars(m)
-		resp.Write(b)
+		if _, err = resp.Write(b); err != nil {
+			fmt.Fprint(os.Stderr, err.Error())
+		}
+		b = bytes.ReplaceAll(b, []byte("\n"), []byte("\\n"))
+		fmt.Println(lp + "::" + req.RequestURI + "::body::" + string(b))
 	}
 }
 
@@ -241,4 +255,13 @@ func fillVars(m *Mock) []byte {
 	}
 
 	return ret
+}
+
+func randString(n int) string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
